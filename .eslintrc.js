3bd2c58eebef18f28ea1fb9e7fe6c7017f4456c2
Kris Lubiniecki <klubiniecki@gmail.com>
2021-02-22 16:53:24 +1100

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+  },
+  overrides: [
+    {
+      files: ["cypress/**/*"],
+      plugins: ["cypress"],
+      env: {
+        "cypress/globals": true,
+      },
+      rules: {
+        "import/no-extraneous-dependencies": 0,
+      },
+    },
+  ],
+};
